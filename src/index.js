b38d24b19871725c878badb1f4061f8d7dd28abb
Carlos Melegrito <cj@mlgrto.com>
2020-08-30 15:54:15 +1000

    Display a map

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import mapboxgl from 'mapbox-gl'
 import AdaWidgetSDK from '@ada-support/ada-widget-sdk'
 
 const widgetSDK = new AdaWidgetSDK()
@@ -11,5 +12,17 @@ widgetSDK.init(event => {
   if (widgetSDK.widgetIsActive) {
     widgetSDK.sendUserData({}, () => {})
   }
+
+  // Set up map
+  mapboxgl.accessToken = token
+  const map = new mapboxgl.Map({
+    container: 'map-container',
+    style: 'mapbox://styles/mapbox/streets-v11',
+  })
+
+  // Remove loading indicator once map is loaded
+  map.on('load', () => {
+    document.getElementById('loading').remove()
+  })
 
 })
